45e7e64a091358a0874dc558867460e8570ea203
Gwenhael Le moine <gwenhael.le.moine@gmail.com>
2010-08-10 22:07:26 +0700

    applied theme to header

--- header.php
+++ header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 	<head>
+    <link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/images/favicon.ico" />
 
 		<!-- "H5": The HTML-5 WordPress Template Theme -->
 		<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>">
@@ -16,17 +17,34 @@
 		<?php wp_enqueue_script('jquery'); ?>
 		<?php wp_head(); ?>
 
-		<script src="<?php bloginfo('template_directory'); ?>/javascript/h5.js"></script>
+    <!--[if IE]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    <!-- https://code.google.com/p/ie7-js/ -->
+    <!--[if lt IE 9]>
+        <script src="http://ie7-js.googlecode.com/svn/trunk/lib/IE9.js"></script>
+        <![endif]-->
 
 	</head>
 	<body <?php body_class(); ?>>
 
-		<header>
-			<h1><a href="<?php bloginfo('url'); ?>/"><?php bloginfo('name'); ?></a></h1>
-			<h2><?php bloginfo('description'); ?></h2>
-		</header>
+    <div id="content">
+      <header>
+        <h1><a href="<?php bloginfo('url'); ?>/"><img src="<?php bloginfo('template_directory'); ?>/images/logo.png" id="logo" alt="<?php bloginfo('name'); ?>" /></a></h1>
+
+        <div id="subtitle">
+          <div id="icons">
+            <img id="cafe" src="<?php bloginfo('template_directory'); ?>/images/cafe.png" alt="Café" />
+            <img id="books" src="<?php bloginfo('template_directory'); ?>/images/books.png" alt="Books" />
+            <img id="wifi" src="<?php bloginfo('template_directory'); ?>/images/wifi.png" alt="Wifi" />
+            <img id="friends" src="<?php bloginfo('template_directory'); ?>/images/friends.png" alt="Friends" />
+            <img id="lollypops" src="<?php bloginfo('template_directory'); ?>/images/lollypops.png" alt="Lollypops" />
+          </div>
+          <div id="address"><?php bloginfo('description'); ?><!-- 29 Tô Hiến Thành, Hà Nội, Việt Nam --></div>
+        </div>
+      </header>
 		<nav>
-			<ul>
+			<menu>
 				<?php wp_list_pages('title_li='); ?>
-			</ul>
+			</menu>
 		</nav>
