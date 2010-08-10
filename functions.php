511c26c14cc370b8b2405596cb6e5cf42110a763
Gwenhael Le moine <gwenhael.le.moine@gmail.com>
2010-08-10 22:25:05 +0700

    add custom menu support

--- functions.php
+++ functions.php
@@ -1,10 +1,19 @@
 <?php
 if (function_exists('register_sidebar')) {
-	register_sidebar(array(
-		'before_widget' => '<li id="%1$s" class="widget %2$s">',
-		'after_widget' => '</li>',
-		'before_title' => '<h2 class="widgettitle">',
-		'after_title' => '</h2>',
-	));
+   register_sidebar(array(
+                       'before_widget' => '<li id="%1$s" class="widget %2$s">',
+                       'after_widget' => '</li>',
+                       'before_title' => '<h2 class="widgettitle">',
+                       'after_title' => '</h2>',
+                       ));
+}
+
+add_action( 'init', 'register_my_menus' );
+function register_my_menus() {
+   register_nav_menus(
+      array(
+         'menu' => __( 'Menu' )
+         )
+      );
 }
 ?>
