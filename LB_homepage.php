41d62663e5157392ba74fa69e07ce75caf5c1758
Gwenhael Le moine <gwenhael.le.moine@gmail.com>
2010-08-14 17:08:53 +0700

    use image title to build link to service

--- LB_homepage.php
+++ LB_homepage.php
@@ -61,7 +61,7 @@
           $media_items = get_attachments_by_media_tags( 'media_tags=service' );
 if ($media_items) {
    foreach ($media_items as $media_item) {
-        echo '<a href="#">';
+      echo '<a href="' . $media_item->post_title . '">'; // TODO: define exactly url
         echo '<img src="' . wp_get_attachment_url($media_item->ID) . '" />';
         echo '<p>';
         if ( !empty( $media_item->post_excerpt ) ) echo $media_item->post_excerpt;
